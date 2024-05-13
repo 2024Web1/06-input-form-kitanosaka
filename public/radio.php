3c5282d0a82dd275d3d80661de80148294400607
kitanosaka <kd1364667@st.kobedenshi.ac.jp>
2024-05-13 14:15:55 +0900

    0513

--- public/radio.php
+++ public/radio.php
@@ -4,15 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>input.php</title>
+    <title>radio.php</title>
 </head>
 
 <body>
     <h4>0J03010 坂根眞人</h4>
+    <p>ラジオボタン</p>
     <?php
-    echo '<p>' . $_POST['name'] . 'さんの年齢は' . $_POST['age'] . '才です。</p>';
+    echo '<p>あなたの好きな果実は、' . $_PPOST['fruit'] . 'ですね。</p>';
     ?>
-    <a href='input.html'>戻る</a>
+    <a href='radio.html'>戻る</a>
 </body>
 
 </html>
